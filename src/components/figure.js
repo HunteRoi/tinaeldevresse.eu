6fa80882f612f4603497b2e8dfb7c886b44bb50f
HunteRoi <tinael.devresse@hotmail.com>
2020-05-28 01:21:35 +0200

    Add Figure component to embed web objects with a caption

---
+++ src/components/figure.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import styles from './figure.module.css';
+
+function getTagAccordingToDataType(data) {
+  if (typeof data !== 'object' || !data.type) return <></>;
+
+  switch (data.type) {
+    case 'image':
+      return <img {...data} />;
+
+    case 'video':
+      return (<video controls width={data.width}>
+        {
+          data.sources && Array.isArray(data.sources)
+            ? data.sources.map(s => <source src={s.src} type={s.type} />)
+            : <source {...data} />
+        }
+      </video>);
+
+    case 'audio':
+      return (<audio controls width={data.width}>
+        {
+          data.sources && Array.isArray(data.sources) 
+            ? data.sources.map((s) => <source src={s.src} type={s.type} />)
+            : <source {...data} />
+        }
+      </audio>);
+
+    case 'html':
+      return (
+        <div className={styles.container}>
+          <iframe {...data}></iframe>
+        </div>
+      );
+
+    default: return <object data={data.value} width={data.width} height={data.height}></object>
+  }
+}
+
+export default function Figure({data, children, style}) {
+  if (!data && !children) return <></>;
+
+  return (
+    <figure>
+      {data && getTagAccordingToDataType(data)}
+      
+      <figcaption className={styles.legend}>{children}</figcaption>
+    </figure>
+  );
+};

ddc9de447a94a495dc5f7e2a52b6931bd2a4eb06
HunteRoi <me@tinaeldevresse.eu>
2021-11-27 20:46:18 +0100

    new post sketch, in french

---
+++ src/components/AnchoredHeading.js
@@ -0,0 +1,3 @@
+import Heading from '@theme/Heading';
+export const AnchoredH2 = Heading('h2');
+export const AnchoredP = Heading('p');
--- src/components/figure.js
+++ src/components/figure.js
@@ -2,49 +2,67 @@ import React from 'react';
 import styles from './figure.module.css';
 
 function getTagAccordingToDataType(data) {
-  if (typeof data !== 'object' || !data.type) return <></>;
+	if (typeof data !== 'object' || !data.type) return <></>;
 
-  switch (data.type) {
-    case 'image': case 'img':
-      return <img {...data} />;
+	switch (data.type) {
+		case 'image':
+		case 'img':
+			return data.url ? (
+				<a href={data.url} target='_blank'>
+					<img {...data} />
+				</a>
+			) : (
+				<img {...data} />
+			);
 
-    case 'video':
-      return (<video controls width={data.width}>
-        {
-          data.sources && Array.isArray(data.sources)
-            ? data.sources.map(s => <source src={s.src} type={s.type} />)
-            : <source {...data} />
-        }
-      </video>);
+		case 'video':
+			return (
+				<video controls width={data.width}>
+					{data.sources && Array.isArray(data.sources) ? (
+						data.sources.map((s) => <source src={s.src} type={s.type} />)
+					) : (
+						<source {...data} />
+					)}
+				</video>
+			);
 
-    case 'audio':
-      return (<audio controls width={data.width}>
-        {
-          data.sources && Array.isArray(data.sources) 
-            ? data.sources.map((s) => <source src={s.src} type={s.type} />)
-            : <source {...data} />
-        }
-      </audio>);
+		case 'audio':
+			return (
+				<audio controls width={data.width}>
+					{data.sources && Array.isArray(data.sources) ? (
+						data.sources.map((s) => <source src={s.src} type={s.type} />)
+					) : (
+						<source {...data} />
+					)}
+				</audio>
+			);
 
-    case 'html':
-      return (
-        <div className={styles.container} align={data.align}>
-          <iframe {...data}></iframe>
-        </div>
-      );
+		case 'html':
+			return (
+				<div className={styles.container} align={data.align}>
+					<iframe {...data}></iframe>
+				</div>
+			);
 
-    default: return <object data={data.value} width={data.width} height={data.height}></object>
-  }
+		default:
+			return (
+				<object
+					data={data.value}
+					width={data.width}
+					height={data.height}
+				></object>
+			);
+	}
 }
 
-export default function Figure({data, children}) {
-  if (!data && !children) return <></>;
+export default function Figure({ data, children }) {
+	if (!data && !children) return <></>;
 
-  return (
-    <figure>
-      {data && getTagAccordingToDataType(data)}
+	return (
+		<figure>
+			{data && getTagAccordingToDataType(data)}
 
-      <figcaption className={styles.legend}>{children}</figcaption>
-    </figure>
-  );
-};
+			<figcaption className={styles.legend}>{children}</figcaption>
+		</figure>
+	);
+}

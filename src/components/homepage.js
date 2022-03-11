306f17b9790520e45b669ea15e14a83ab89e0102
HunteRoi <me@tinaeldevresse.eu>
2022-03-11 17:20:31 +0100

    fix logo and change photo

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -7,25 +7,25 @@ import Newsletter from './newsletter';
 import config from '../../website_config.json';
 
 export default function HomePage({ title, tagline, descriptionLines }) {
+    return (
+        <section className={styles.hero}>
+            <Link
+                className={classnames('footer__link-item', styles.title)}
+                to='/blog'
+            >
+                <img src={require('@site/static/img/me.jpg').default} />
+                <span>{title}</span>
+            </Link>
+            <span className={styles.subtitle}>{tagline}</span>
+            <hr className={styles.divider} />
+            <div className={styles.description}>
+                {descriptionLines &&
+                    descriptionLines.map((d, i) => <p key={i}>{d}</p>)}
+            </div>
 
-  return (
-    <section className={styles.hero}>
-      <Link
-        className={classnames('footer__link-item', styles.title)}
-        to='/blog'
-      >
-        <img src={require('@site/static/img/me.jpg').default} />
-        <span>{title}</span>
-      </Link>
-      <span className={styles.subtitle}>{tagline}</span>
-      <hr className={styles.divider} />
-      <div className={styles.description}>
-        {descriptionLines && descriptionLines.map((d, i) => <p key={i}>{d}</p>)}
-      </div>
-
-      <div className={styles.description}>
-        <Newsletter url={config.MAILCHIMP_URL} />
-      </div>
-    </section>
-  );
+            <div className={styles.description}>
+                <Newsletter url={config.MAILCHIMP_URL} />
+            </div>
+        </section>
+    );
 }
